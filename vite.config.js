bd3094bb7c66a85ba9ff84309626dbc1372a6946
Saran Hiruthik M <saran.hiruthik83@gmail.com>
2025-02-09 10:40:21 +0530

    Fix: disable HMR in production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  server: {
+    hmr: mode === "development" ? true : false,  // Disable HMR in production
+  },
+  preview: {
+    hmr: false,  // Ensure preview mode doesn’t use HMR
+  }
+}))
